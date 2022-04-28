13e162b1106772d9a5f50e3e9159859d5b0f5040
CYI1995 <90730278+CYI1995@users.noreply.github.com>
2022-04-28 11:21:48 -0700

    Update Uhrig_simulation.py

--- Uhrig_simulation.py
+++ Uhrig_simulation.py
@@ -82,6 +82,10 @@ def matrix_norm(M,dim):
 def filter(j,N):
     return math.sin(j*math.pi/(2*N))**2
 
+def Trotter_2nd(H1,H2,dt):
+
+    return scipy.linalg.expm(-1j*H1*dt/2).dot(scipy.linalg.expm(-1j*H2*dt).dot(scipy.linalg.expm(-1j*H1*dt/2)))
+
 site = 4
 dim = 2**site
 scale = -1
@@ -96,18 +100,19 @@ H_diff = X - Z
 # H0 = X
 # H_diff = Z
 
-t0 = 0.001
-M = 10000
+t0 = 0.01
+M = 1000
 X_axis = np.zeros(M)
 Y1_axis = np.zeros(M)
 Y2_axis = np.zeros(M)
 Y3_axis = np.zeros(M)
+Y4_axis = np.zeros(M)
 
 # U_dd is Uhrig's dynamical decoupling of a single period.
 # U_id is ideal evolution of a single dt.
 U_id = scipy.linalg.expm(-1j*H0*t0)
 
-N1 = 2
+N1 = 4
 U_dd1 = np.identity(dim,dtype = complex)
 for j in range((N1)):
     dt = (filter(j+1,N1) - filter(j,N1))*t0
@@ -119,7 +124,7 @@ for j in range((N1)):
     U_temp = scipy.linalg.expm(-1j*H_temp*dt)
     U_dd1 = U_temp.dot(U_dd1)
 
-N2 = 4
+N2 = 6
 U_dd2 = np.identity(dim,dtype = complex)
 for j in range((N2)):
     dt = (filter(j+1,N2) - filter(j,N2))*t0
@@ -131,7 +136,7 @@ for j in range((N2)):
     U_temp = scipy.linalg.expm(-1j*H_temp*dt)
     U_dd2 = U_temp.dot(U_dd2)
 
-N3 = 6
+N3 = 8
 U_dd3 = np.identity(dim,dtype = complex)
 for j in range((N3)):
     dt = (filter(j+1,N3) - filter(j,N3))*t0
@@ -143,24 +148,35 @@ for j in range((N3)):
     U_temp = scipy.linalg.expm(-1j*H_temp*dt)
     U_dd3 = U_temp.dot(U_dd3)
 
+p = 1/(4 - 4**(1/3))
+Trotter_4th = Trotter_2nd(X,Z,p*t0).dot(Trotter_2nd(X,Z,p*t0))
+Trotter_4th = Trotter_2nd(X,Z,(1-4*p)*t0).dot(Trotter_4th)
+Trotter_4th = (Trotter_2nd(X,Z,p*t0).dot(Trotter_2nd(X,Z,p*t0))).dot(Trotter_4th)
+# Trotter_4th = Trotter_2nd(X,Z,t0)
+
 U0 = np.identity(dim,dtype = complex)
 U1 = np.identity(dim,dtype = complex)
 U2 = np.identity(dim,dtype = complex)
 U3 = np.identity(dim,dtype = complex)
+U4 = np.identity(dim,dtype = complex)
 
 for m in range(M):
     U0 = U_id.dot(U0)
     U1 = U_dd1.dot(U1)
     U2 = U_dd2.dot(U2)
     U3 = U_dd3.dot(U3)
+    U4 = Trotter_4th.dot(U4)
 
     X_axis[m] = (m+1)*t0
     Y1_axis[m] = matrix_norm(U1 - U0,dim)
     Y2_axis[m] = matrix_norm(U2 - U0,dim)
     Y3_axis[m] = matrix_norm(U3 - U0,dim)
+    Y4_axis[m] = matrix_norm(U4 - U0,dim)
 
-# plt.scatter(X_axis,Y1_axis,label = 'N=2')
-plt.scatter(X_axis,Y2_axis,label = 'N=4')
-plt.scatter(X_axis,Y3_axis,label = 'N=6')
+plt.plot(X_axis,Y1_axis,label = 'N=4')
+plt.plot(X_axis,Y2_axis,label = 'N=6')
+plt.plot(X_axis,Y3_axis,label = 'N=8')
+plt.plot(X_axis,Y4_axis,label = '4th Totter')
 plt.legend()
 plt.show()
+
